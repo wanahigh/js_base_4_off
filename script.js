576985263bceee6c2cd059a30ed40e6d528169c4
wanahigh <fabienruault@gmail.com>
2017-05-30 12:51:14 +0200

    maj base 4

---
+++ script.js
@@ -0,0 +1,54 @@
+
+var tab = [
+    {
+        question: 'quel age du commandant ?',
+        reponses1: '42',
+        reponses2: '87',
+        reponses3: '65',
+        reponses4: '26'
+    },
+    {
+        question: 'quel age a la terre ?',
+        reponses1: '13 milliard d.année',
+        reponses2: '80K ans',
+        reponses3: '20 000 lieu sous les mer',
+        reponses4: 'gerard'
+
+    },
+    {
+        question: 'la durée de vie ?',
+        reponses1: '13 milliard d.année',
+        reponses2: '80K ans',
+        reponses3: '20 000 lieu sous les mer',
+        reponses4: 'gerard'
+    }
+
+] ;
+
+function StyleCss() {
+
+
+    for (var i=0 ; i < tab.length ;i++) {
+        if((tab[i].type).includes("css")){
+            var livres=document.getElementById('book').innerHTML += tab[i].title
+                +"</br>"+tab[i].author+"</br>"+tab[i].link+"</br>"+tab[i].type;
+        }
+    }
+}
+
+
+
+
+function StyleJS() {
+
+
+    for (var i=0 ; i < tab.length ;i++) {
+        if((tab[i].type).includes("js")){
+
+
+            var livres=document.getElementById('book').innerHTML += tab[i].title
+                +"</br>"+tab[i].author+"</br>"+tab[i].link+"</br>"+tab[i].type;
+        }
+    }
+}
+
