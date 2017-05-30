12e5063f6793979af5106202e436c879f6301495
wanahigh <fabienruault@gmail.com>
2017-05-30 22:44:16 +0200

    bouton fonctionnel

--- script.js
+++ script.js
@@ -1,59 +1,74 @@
 
-var tab  = [
+var quizz  = [
     {
-        "question" : "Qui a inventé la dynamite?",
-        "choix"  : [
-            "John Trinit",
-            "Paul Craine",
-            "Alfred Nobel",
-            "John Dynamite"
-        ],
-        "correct" : "Alfred Nobel"
-    },
-    {
-        "question" :"Qui a inventé le téléphone?",
-        "choix"       :   [
-            "Alexander Graham Bell",
-            "Benjamin Franklin",
-            "Thomas Edison",
-            "Ma femme"
-        ],
-        "correct" : "Thomas Edison"
-    },
-    {
-        "question" :"A qui doit-on la pénicilline?",
-        "choix"       :   [
-            "Sir Alexander Fleming",
-            "Louis Pasteur",
-            "Antoine Lavoisier",
-            "Alfred Pénicilline"
-        ],
-        "correct" : "Sir Alexander Fleming"
-    },
-    {
-        "question" :"Qui a inventé la boîte de conserve?",
-        "choix"       :   [
-            " Joseph Cugnot",
-            "Auguste Denayrouze",
-            "Cédan Laboite",
-            "Nicolas Appert"
-        ],
-        "correct" : "Nicolas Appert"
-    }    ];
+        question : "Qui a inventé la dynamite?",
 
- {
-  function QR() {
-      for (var i = 0; i < tab.length; i++)
-        document.getElementById('quest').innerHTML = tab[i].question;
-        document.getElementById('bt1').innerHTML = tab[i].choix[0];
-        document.getElementById('bt22').innerHTML = tab[i].choix[1];
-        document.getElementById('bt3').innerHTML = tab[i].choix[2];
-        document.getElementById('bt4').innerHTML = tab[i].choix[3];
+        reponse1: "John Trinit",
+        reponse2:"Paul Craine",
+        reponse3:  "Alfred Nobel",
+        reponse4:"John Dynamite",
+        correct : "Alfred Nobel"
+    },
+    {
+        question :"Qui a inventé le téléphone?",
+
+        reponse1:"Alexander Graham Bell",
+        reponse2:"Benjamin Franklin",
+        reponse3:"Thomas Edison",
+        reponse4:"Ma femme",
+        correct : "Thomas Edison"
+    },
+    {
+        question :"A qui doit-on la pénicilline?",
+
+        reponse1:"Sir Alexander Fleming",
+        repons2:"Louis Pasteur",
+        reponse3:"Antoine Lavoisier",
+        reponse4:"Alfred Pénicilline",
+        correct: "Sir Alexander Fleming"
     }
-}
 
+ ];
 
+     var i=0;
+     document.getElementById('bt2').onclick = function() {
+         document.getElementById('quest').innerHTML = null;
+         document.getElementById('bt1').innerHTML = null;
+         document.getElementById('bt22').innerHTML = null;
+         document.getElementById('bt3').innerHTML = null;
+         document.getElementById('bt4').innerHTML = null;
+         document.getElementById('quest').innerHTML += quizz[i].question;
+         console.log(i);
+         document.getElementById('bt1').innerHTML += quizz[i].reponse1;
+         document.getElementById('bt22').innerHTML += quizz[i].reponse2;
+         document.getElementById('bt3').innerHTML += quizz[i].reponse3;
+         document.getElementById('bt4').innerHTML += quizz[i].reponse4;
+         if (i < 4) {
+             i++;
+         }
+         else {
+             i=4;
+         }
+     };
 
-
-
+var i=0;
+document.getElementById('bt44').onclick = function() {
+    document.getElementById('quest').innerHTML = null;
+    document.getElementById('bt1').innerHTML = null;
+    document.getElementById('bt22').innerHTML = null;
+    document.getElementById('bt3').innerHTML = null;
+    document.getElementById('bt4').innerHTML = null;
+    document.getElementById('quest').innerHTML += quizz[i].question;
+    console.log(i);
+    document.getElementById('bt1').innerHTML += quizz[i].reponse1;
+    document.getElementById('bt22').innerHTML += quizz[i].reponse2;
+    document.getElementById('bt3').innerHTML += quizz[i].reponse3;
+    document.getElementById('bt4').innerHTML += quizz[i].reponse4;
+    if (i < 4) {
+        i++;
+    }
+    else {
+        i=4;
+    }
+};
 
